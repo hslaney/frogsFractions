5114b5dc999245df178d39bb93cd03a55c652dca
hslaney <haley.slaney@gmail.com>
2020-10-13 19:12:46 -0400

    Initial commit

---
+++ src/com/company/Frog.java
@@ -0,0 +1,108 @@
+package com.company;
+
+public class Frog {
+    private String name;
+    private int xPos;
+    private int yPos;
+
+    public Frog(String n,int x, int y)		// constructor
+    {
+        name = n;
+        xPos=x;
+        yPos=y;
+        System.out.println(name);
+    }
+    public void setPos(int x, int y)	// accessor methods
+    {
+        xPos = x;
+        yPos = y;
+    }
+    public int getXPos()
+    {
+        return xPos;
+    }
+    public int getYPos()
+    {
+        return yPos;
+    }
+    public void moveWest()		// change the pos of the frog
+    {
+        xPos = xPos-1;
+    }
+    public void moveEast()
+    {
+        xPos = xPos + 1;
+    }
+    public void moveNorth()
+    {
+        yPos=yPos+1;
+    }
+    //TODO jump method move up and to the right
+    //This method should take an integer argument.  This argument controls how many spaces
+    // the frog will move both up and right.  Returns no value.
+    public void jump(int space)
+    {
+        xPos+=space;
+        yPos+=space;
+
+    }
+
+    //TODO visit method moves frog to the location of the friend frog
+    // This method will take an argument of type Frog.  It will find the position of the
+    // frog and move to this position.  Returns no value.
+    public void visit(Frog other){
+        int x = other.getXPos();
+        int y = other.getYPos();
+        this.xPos = x;
+        this.yPos = y;
+    }
+
+    public void eatFly()
+    {
+        System.out.println("Yum");
+    }
+    public String toString()
+    {
+        String s = "Hi I am "+ name +" the frog and I live at "+ xPos + " " + yPos;
+        return s;
+    }
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Frog f1 = new Frog("Freddy",3,4);	// create an instance of Frog
+        f1.eatFly();						// call Frog methods
+        f1.moveNorth();
+        System.out.println(f1);
+
+        Frog f2 = new Frog("Francine", -2,0);
+        f2.eatFly();
+        // TODO uncomment these to test new code.
+        f2.jump(2);
+        f1.visit(f2);
+        System.out.println(f1);
+        System.out.println(f2);
+
+        // make a new frog named Frank.  have him jump twice and then go visit Freddy.
+        Frog f3 = new Frog("Frank", -4, 4);
+        f3.jump(2);
+        System.out.println(f3);
+        f3.visit(f1);
+        System.out.println(f3);
+
+    }
+
+}
+
+/*  Output
+Freddy
+Yum
+Hi I am Freddy the frog and I live at 3 5
+Francine
+Yum
+Hi I am Freddy the frog and I live at 0 2
+Hi I am Francine the frog and I live at 0 2
+Frank
+Hi I am Frank the frog and I live at -2 6
+Hi I am Frank the frog and I live at 0 2
+*/
